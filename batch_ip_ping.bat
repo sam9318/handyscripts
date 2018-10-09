8d91b4eb60fb09f77c68f58dcd1eec50bb4df62f
Infosecnerd9 <32172749+sam9318@users.noreply.github.com>
2018-10-09 11:59:04 +0530

    Add files via upload
    Handy script to ping multiple ips as well as resolve hostnames during infra security testing. Just change the output file path as per your local system

---
+++ batch_ip_ping.bat
@@ -0,0 +1,2 @@
+ping www.howtogeek.com >C:\Users\samratdas\Desktop\test.txt
+ping www.komando.com >>C:\Users\samratdas\Desktop\test.txt
---
+++ hostnamelookup.bat
@@ -0,0 +1,2 @@
+nslookup -type=ptr 127.0.0.1 >C:\Users\samratdas\Desktop\hostname.txt
+nslookup -type=ptr 127.0.0.0 >>C:\Users\samratdas\Desktop\hostname.txt
